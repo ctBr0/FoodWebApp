e5413c6ac61bc8b9e3ef6d89280df5107cbde707
gr1tHub <justin60008@gmail.com>
2022-04-02 22:14:48 -0700

    correct FoodItem.java

--- application/FoodItem.java
+++ application/FoodItem.java
@@ -1,50 +1,77 @@
 package application;
 
+public class FoodItem
+{
 
-import java.time.Duration;
+    private String name;
+    private double price;
+    private int prepTimeInMins;
+    private String category;
+    private String[] ingredients;
+    FoodItem nextItem;
+    int itemNumber;
 
-public class FoodItem {
+    public FoodItem(String name, double price, int prepTimeInMins, String category, String[] ingredients)
+    {
 
-	    String foodName,picture,description;
-	    int price;
-	    int numberoffood;
-	    
-		public FoodItem(String foodName, String picture, String description, int price, int numberoffood) {
-			super();
-			this.foodName = foodName;
-			this.picture = picture;
-			this.description = description;
-			this.price = price;
-			this.numberoffood = numberoffood;
-		}
-		public String getName() {
-			return foodName;
-		}
-		public void setName(String foodName) {
-			this.foodName = foodName;
-		}
-		public String getPicture() {
-			return picture;
-		}
-		public void setPicture(String picture) {
-			this.picture = picture;
-		}
-		public String getDescription() {
-			return description;
-		}
-		public void setDescription(String description) {
-			this.description = description;
-		}
-		public double getPrice() {
-			return price;
-		}
-		public void setPrice(int price) {
-			this.price = price;
-		}
-		public int getNumberoffood() {
-			return numberoffood;
-		}
-		public void setNumberoffood(int numberoffood) {
-			this.numberoffood = numberoffood;
-		}	    
+        this.name = name;
+        this.price = price;
+        this.prepTimeInMins = prepTimeInMins;
+        this.category = category;
+        this.ingredients = ingredients;
+        this.nextItem = null;
+        this.itemNumber = 0;
+
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public int getPrepTime()
+    {
+        return prepTimeInMins;
+    }
+
+    public String getCategory()
+    {
+        return category;
+    }
+
+    public String[] getIngredients()
+    {
+        return ingredients;
+    }
+
+    public void setName(String newName)
+    {
+        this.name = newName;
+    }
+
+    public void setPrice(double newPrice)
+    {
+        this.price = newPrice;
+    }
+
+    public void setPrepTime(int newPrepTimeInMins)
+    {
+        this.prepTimeInMins = newPrepTimeInMins;
+    }
+
+    public void setCategory(String newCategory)
+    {
+        this.category = newCategory;
+    }
+
+    public void setIngredients(String[] newIngredients)
+    {
+        this.ingredients = newIngredients;
+    }
+    
 }
