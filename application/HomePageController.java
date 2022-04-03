26898346b5770e5b20f415da01013c4384bfc3ac
yiwillz <99515283+yiwillz@users.noreply.github.com>
2022-04-03 13:36:10 -0700

    Update HomePageController.java

--- application/HomePageController.java
+++ application/HomePageController.java
@@ -16,6 +16,8 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 public class HomePageController implements Initializable{
@@ -37,21 +39,33 @@ public class HomePageController implements Initializable{
 
     @FXML
     private TableColumn<FoodItem, Integer> numberoffood;
-	ObservableList<FoodItem> list = FXCollections.observableArrayList(
-	new FoodItem("pizze", "*", "cheese,meat,veges", 9, 0),
-    new FoodItem("buger", "*", "meat,bread,cheese,veges", 10, 0),
-	new FoodItem("xiaolongbao", "*", "dough,meat", 10, 0),
-	new FoodItem("noodles", "*", "dough", 11, 0));
+    
+    private final ObservableList<FoodItem> list = FXCollections.observableArrayList();
 	
    @Override
     public void initialize(URL url, ResourceBundle rb) {
 
     	
     	name.setCellValueFactory(new PropertyValueFactory<FoodItem,String>("name"));
-    	picture.setCellValueFactory(new PropertyValueFactory<FoodItem,String>("picture"));
+    	picture.setPrefWidth(80);
+    	picture.setCellValueFactory(new PropertyValueFactory<>("picture"));
+    	
     	description.setCellValueFactory(new PropertyValueFactory<FoodItem,String>("description"));
     	price.setCellValueFactory(new PropertyValueFactory<FoodItem,Integer>("price"));
-    	numberoffood.setCellValueFactory(new PropertyValueFactory<FoodItem,Integer>("numberoffood"));	
+    	numberoffood.setCellValueFactory(new PropertyValueFactory<FoodItem,Integer>("numberoffood")); 
+    	
+    	ImageView pizza = new ImageView(new Image(this.getClass().getResourceAsStream("pizza.png")));
+    	FoodItem f1 =new FoodItem("pizza",pizza, "cheese,meat,veges", 9, 0);
+    	list.add(f1);
+    	ImageView burger = new ImageView(new Image(this.getClass().getResourceAsStream("burger.png")));
+   	    ImageView xiaolongbao = new ImageView(new Image(this.getClass().getResourceAsStream("xiaolongbao.png")));
+   	    ImageView noodle = new ImageView(new Image(this.getClass().getResourceAsStream("noodle.png")));
+   	    FoodItem f2 =new FoodItem("buger",burger, "meat,bread,cheese,veges", 10, 0);
+   	    FoodItem f3 =new FoodItem("xiaolongbao", xiaolongbao, "dough,meat", 10, 0);
+   	    FoodItem f4 =new FoodItem("noodles", noodle, "dough", 11, 0);
+   	    list.add(f2);
+   	    list.add(f3);
+   	    list.add(f4);
     	table.setItems(list);
     }
     @FXML
