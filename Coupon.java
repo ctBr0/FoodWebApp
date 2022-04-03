f4e45fa871e752f38fa83e6c8f32c0e2128ef854
gr1tHub <justin60008@gmail.com>
2022-04-02 17:13:44 -0700

    edits

---
+++ Coupon.java
@@ -0,0 +1,19 @@
+public class Coupon {
+    
+    private double discountPerc; //Discount percentage: 0.4 - 40% off
+
+    public Coupon(double discountPerc)
+    {
+        this.discountPerc = discountPerc;
+    }
+
+    public void setDiscountPerc(int newPerc)
+    {
+        this.discountPerc = newPerc;
+    }
+
+    public double getDiscountPerc()
+    {
+        return this.discountPerc;
+    }
+}
