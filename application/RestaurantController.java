d3754682586d0a86e95ce920fba6e97ffb98c715
Dollars7 <99517710+Dollars7@users.noreply.github.com>
2022-04-03 15:54:55 -0700

    Add files via upload

--- application/RestaurantController.java
+++ application/RestaurantController.java
@@ -44,6 +44,13 @@ public class RestaurantController {
 
     @FXML
     void rowClicked(MouseEvent event) {
+        Menu clickedMenu = Menu.getSelectionModel().getSelectedItem();
+        inputFoodId.setText(String.valueOf(menu.getId()));
+        inputFoodName.setText(String.valueOf(menu.getName()));
+        inputPrice.setText(String.valueOf(menu.getPrice()));
+        inputPreTime.setText(String.valueOf(menu.getPrepTime()));
+        inputCategory.setText(String.valueOf(menu.getCategory()));
+        // inputIngredients.setText(String.valueOf(menu.getIngredients()));
 
     }
 
@@ -51,5 +58,25 @@ public class RestaurantController {
     void submit(ActionEvent event) {
 
     }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        idCol.setCellValueFactory(new PropertyValueFactory<FoodItem, Integer>("Item number"));
+        typeCol.setCellValueFactory(new PropertyValueFactory<FoodItem, String>("Name"));
+        nameCol.setCellValueFactory(new PropertyValueFactory<FoodItem, String>("Price"));
+        preTCol.setCellValueFactory(new PropertyValueFactory<FoodItem, Integer>("Preparation Time"));
+        cateCol.setCellValueFactory(new PropertyValueFactory<FoodItem, String>("Category"));
+        // nameCol.setCellValueFactory(new PropertyValueFactory<FoodItem, String>("Ingredients"));
+        setupTable();
+    }
+
+
+    private void setupTable(){
+        FoodlList list = new FoodList("");
+
+        // animals.getItems().addAll(menu1,men2,menu3);
+        
+    }
+
+
 
 }
